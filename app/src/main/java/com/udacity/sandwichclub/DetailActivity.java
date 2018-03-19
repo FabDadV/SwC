16f3d3324ac61f3b408c47e75b8c38c5079d4fc7
FabDadV <fabdadv@gmail.com>
2018-03-19 21:49:38 +0700

    Pass Parameter from MainActivity to DetailFragment

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -7,15 +7,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
-import com.udacity.sandwichclub.model.Sandwich;
-import com.udacity.sandwichclub.utils.JsonUtils;
-
-import java.util.List;
-
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
-    private static final int DEFAULT_POSITION = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +24,9 @@ public class DetailActivity extends AppCompatActivity {
                     .commit();
         }
 
-
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
-        }
-
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
-        if (position == DEFAULT_POSITION) {
-            // EXTRA_POSITION not found in intent
-            closeOnError();
-            return;
         }
     }
 
